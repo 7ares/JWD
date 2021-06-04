5804f484545c6a61c3e6d68252308b5d505feb1b
7ares mohamed <7aresmohamed@gmail.com>
2021-06-05 01:40:04 +0200

    initial commit

---
+++ src/main/java/com/example/JWD/JwdApplication.java
@@ -0,0 +1,13 @@
+package com.example.JWD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JWD/JwdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JWD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
